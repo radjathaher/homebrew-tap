2d9dca1a53989f1d79605ce803486d7be8e02561
Radja Thaher <radjapolem@gmail.com>
2026-01-26 21:15:37 +0700

    feat: add linear-cli

---
+++ Formula/linear-cli.rb
@@ -0,0 +1,25 @@
+class LinearCli < Formula
+  desc "Linear CLI"
+  homepage "https://github.com/radjathaher/linear-cli"
+  version "0.1.1"
+  license "MIT"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/radjathaher/linear-cli/releases/download/v0.1.1/linear-cli-0.1.1-darwin-aarch64.tar.gz"
+      sha256 "11e8aec44c4561b176f234e8ae18a0bf888e3ebb33dc7bc45ace0d88f98a70ed"
+    else
+      url "https://github.com/radjathaher/linear-cli/releases/download/v0.1.1/linear-cli-0.1.1-darwin-x86_64.tar.gz"
+      sha256 "__REPLACE_SHA_DARWIN_X86_64__"
+    end
+  end
+
+  on_linux do
+    url "https://github.com/radjathaher/linear-cli/releases/download/v0.1.1/linear-cli-0.1.1-linux-x86_64.tar.gz"
+    sha256 "__REPLACE_SHA_LINUX_X86_64__"
+  end
+
+  def install
+    bin.install "linear"
+  end
+end
