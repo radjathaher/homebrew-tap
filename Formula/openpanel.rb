3bf9c37e935d36a831e6c6375095f8830b77ce33
Radja Thaher <radjapolem@gmail.com>
2026-01-27 00:57:19 +0700

    feat: bump openpanel to v0.2.0

--- Formula/openpanel.rb
+++ Formula/openpanel.rb
@@ -1,12 +1,12 @@
 class Openpanel < Formula
   desc "OpenPanel CLI"
   homepage "https://github.com/radjathaher/openpanel-cli"
-  version "0.1.0"
+  version "0.2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/radjathaher/openpanel-cli/releases/download/v0.1.0/openpanel-macos-arm64"
-      sha256 "51a62a7281df777a71adceb229fa2d9bf423f5cbbaee523dda2643783b3657db"
+      url "https://github.com/radjathaher/openpanel-cli/releases/download/v0.2.0/openpanel-macos-arm64"
+      sha256 "8f89e4f1dda7fbac91c8751685b89e334707b3eac80b323ee48adf62c1dad489"
     else
       odie "openpanel is only packaged for macOS arm64"
     end
