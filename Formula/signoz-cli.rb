a2d3518e8df96bee2d35802adf563c2da6c09d9a
Radja Thaher <radjapolem@gmail.com>
2026-01-27 12:05:44 +0700

    feat: bump signoz cli to 0.1.1

--- Formula/signoz-cli.rb
+++ Formula/signoz-cli.rb
@@ -1,19 +1,19 @@
 class SignozCli < Formula
   desc "SigNoz CLI"
   homepage "https://github.com/radjathaher/signoz-cli"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/radjathaher/signoz-cli/releases/download/v0.1.0/signoz-0.1.0-darwin-aarch64"
-      sha256 "3dd5174c8e6938ce7a1f07d84b32289ee3f01cedfda36fe9f306846594907c24"
+      url "https://github.com/radjathaher/signoz-cli/releases/download/v0.1.1/signoz-0.1.1-darwin-aarch64"
+      sha256 "121a3f4066edc1df3bee69b95f30320882e6b65aa71ea2d294eb29f19e062c4e"
     else
       odie "signoz-cli is only packaged for macOS arm64"
     end
   end
 
   def install
-    bin.install "signoz-0.1.0-darwin-aarch64" => "signoz"
+    bin.install "signoz-0.1.1-darwin-aarch64" => "signoz"
   end
 end
